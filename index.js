5550c9daf4a1d47d8308c3d3398b1ed121901f88
monsterxcn <monsterxcn@gmail.com>
2020-08-20 21:45:15 +0800

    :bug: window is not defined while using npm package

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /* eslint-disable */
 'use strict'
 
+// Try to fix the error window is not defined while using npm package
+// https://github.com/hellosign/hellosign-embedded/issues/107
+if (typeof window === 'undefined') { global.window = {} }
+
 var Artalk = require('./dist/Artalk.js')
 
 module.exports = Artalk
